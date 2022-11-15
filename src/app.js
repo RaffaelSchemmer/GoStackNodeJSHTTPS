9b46b4357b944994b61cc1c1adac0f0067059fdd
Raffael Bottoli Schemmer <spock2f@gmail.com>
2022-11-15 15:55:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,41 +8,61 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Database (In Memory)
 const repositories = [];
 
+/*
+  GET - Return all repositories (in JSON)
+*/
 app.get("/repositories", (request, response) => {
   
   return response.json(repositories);
 
 });
 
+/*
+  POST - Add new repository that have (id, title, url, techs, likes)
+  Return 201 + json notify that add new repo
+*/
 app.post("/repositories", (request, response) => {
   
   const {title, url , techs} = request.body;
   
   const likes = 0;
-  const project = { id : uuid() , title , url, password , techs, likes };
+  const project = { id : uuid() , title , url, techs, likes};
   repositories.push(project);
   return response.status(201).json(project);
 
 });
 
+/*
+  PUT - Update repo using param id
+  Get all data of body and find id of repository
+  If not find return 400 with error
+  Else return 201 with repository (In JSON)
+*/
 app.put("/repositories/:id", (request, response) => {
   
   const {id} = request.params;
-  const {title, url , password , techs} = request.body;
+  const {title, url , techs} = request.body;
   const repositoriesIndex = repositories.findIndex(project => project.id === id);
   if (repositoriesIndex < 0){
     return response.status(400).json({error:"Repositorie not found!"});  
   }
   
   const likes = repositories[repositoriesIndex].likes;
-  const project = {id, title , url , password , techs , likes};
+  const project = {id, title , url, techs , likes};
   repositories[repositoriesIndex] = project;
   return response.status(201).json(project);
 
 });
 
+/*
+  DELETE - Delete repo using param id
+  Find id of repository
+  If not find return 400 with error
+  Else remove and return 204
+*/
 app.delete("/repositories/:id", (request, response) => {
   
   const {id} = request.params;
@@ -56,6 +76,11 @@ app.delete("/repositories/:id", (request, response) => {
 
 });
 
+/*
+  POST - Like repo using id param
+  If not find return 400 with error
+  Else return 201 with repository (In JSON)
+*/
 app.post("/repositories/:id/like", (request, response) => {
   
   const {id} = request.params;
